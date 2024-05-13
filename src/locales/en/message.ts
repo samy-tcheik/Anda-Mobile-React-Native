6e324b8a17e8a310e5e79fc6abab41e046d5101e
samy tcheikman <samytcheikman@gmail.com>
2024-05-13 20:09:23 +0100

    Improve translatioon text

--- src/locales/en/message.ts
+++ src/locales/en/message.ts
@@ -1,40 +1,40 @@
 export default {
   confirm_email:
-    'Please verify your email address to confirm your registration.',
+    'Please verify your e-mail address to confirm your registration',
   confirm_delete_message:
-    'Are you sure you want to delete your account? This action cannot be undone, and all your data, including your profile information, posts, and settings, will be permanently removed.',
-  delete_my_account: 'Delete My Account',
+    'Are you sure you want to delete your account ? This action cannot be undone, and all your data, including your profile information, posts, and settings, will be permanently removed',
+  delete_my_account: 'Delete my account',
   forget_password_title: 'Forgot password ?',
   forget_password_message:
-    "Enter the email associated with your account and we'll send an email with instructions on ho to recover your password",
-  email_sent_title: 'Email sent',
+    'Enter the e-mail associated with your account and we will send you an e-mail with instructions to recover your password',
+  email_sent_title: 'E-mail sent',
   email_sent_message:
-    'An email containing a verification code has been sent to your email address. Please check your inbox and spam folder.',
-  email_not_received: 'Email not recieved ? ',
+    'An e-mail containing a verification code has been sent to your e-mail address. Please check your inbox and spam folder',
+  email_not_received: 'E-mail not recieved ?',
   code_check_title: 'Verification code',
-  code_check_message: 'Please enter the verification code sent to your email.',
+  code_check_message: 'Please enter the verification code sent to your e-mail',
   password_reset_success_title: 'Password reset successful',
   password_reset_success_message:
     'Your password has been successfully reset. You can now log in with your new password.',
-  create_review_title: 'Write a review',
+  create_review_title: 'Make your opinion known about this place!',
   create_review_message:
     'Leave us a review and share your thoughts! Thank you for being a part of our community.',
   create_review_warning_message:
     'the comments area has been designed to provide additional information about the location, we reserve the right to delete any off-topic comments or even to ban any users posting inappropriate or insulting comments.',
-  rating_input_label: 'How would you rate your experience ?',
-  comment_input_label: 'Do you have any comments or feedback ?',
-  review_success_title: 'Thank You for Sharing Your Experience!',
+  rating_input_label: 'How would you rate your experience?',
+  comment_input_label: 'Add your comment',
+  review_success_title: 'Thank you for sharing your experience!',
   review_success_message:
-    'Your review has been submitted successfully. Thank you for taking the time to share your thoughts with us. Your feedback helps us improve our services for future travelers. We look forward to welcoming you again on your next adventure!',
+    "Your comment has been successfully published. Feel free to post more about places you've already visited to inform other users",
   intro_slider_1_title: 'Welcome to\nAlgeria',
   intro_slider_1_text:
-    'Discover the vibrant culture, breathtaking landscapes, and rich history of Algeria. Let us guide you through an unforgettable journey across this diverse and beautiful country.',
+    "With Anda, Algeria will reveal all its treasures to you, from the most well-known places to those you didn't even suspect existed",
   intro_slider_2_title: 'Explore\nAncient\nTreasures',
   intro_slider_2_text:
-    "Step back in time and explore Algeria's ancient wonders. From the majestic ruins of Timgad to the awe-inspiring desert landscapes of the Sahara, uncover the secrets of civilizations past.",
+    'Organize your outings with a personalized selection of places of interest using the Favorites section',
   intro_slider_3_title: 'Experience\nNatural\nBeauty',
   intro_slider_3_text:
-    "Immerse yourself in Algeria's natural splendor. From the rugged cliffs of the Mediterranean coastline to the tranquil oases of the Sahara, experience the breathtaking beauty of Algeria's diverse landscapes.",
+    'Our application is fully translatable into French, English and Arabic',
   no_search_result_message:
     'No data found, check for spelling or try with other words.',
   no_result_message: 'No data found.',
