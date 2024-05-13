bd533b13828a7828260212dbdf5089bd3ee1161c
samy tcheikman <samytcheikman@gmail.com>
2024-05-13 22:22:58 +0100

    fix update user info

--- src/screens/app/settings/profile/index.tsx
+++ src/screens/app/settings/profile/index.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Keyboard, ScrollView, StyleSheet, Text, View } from 'react-native'
 import AppLayout from '../../app-layout'
 import { NavigationProp } from '@react-navigation/native'
 import { Avatar, Dialog } from '@rneui/base'
@@ -11,7 +11,7 @@ import AppTheme from '../../../../styles'
 import Button from '../../../../components/button'
 import * as ImagePicker from 'react-native-image-picker'
 import { showMessage } from 'react-native-flash-message'
-import { useAuthUser, useUpdateAvatar } from './queries'
+import { useAuthUser, useUpdateAvatar, useUpdateUser } from './queries'
 import { usePopup } from '../../../../hooks/usePopup'
 import ConfirmDeleteDialog from './confirmDelete'
 import Loader from '../../../../components/loader'
@@ -22,15 +22,25 @@ interface ISettingsScreenProps {
 
 const ProfileScreen: React.FC<ISettingsScreenProps> = ({ navigation }) => {
   const { t } = useTranslation()
-  const { data, isLoading } = useAuthUser()
+  const { data, isLoading } = useAuthUser({
+    onSuccess(data) {
+      reset(data)
+    },
+  })
   const confirmDeleteDialog = usePopup()
   const form = useUserForm(data)
   const {
     control,
+    handleSubmit,
+    reset,
     formState: { errors },
   } = form
 
   const { mutate } = useUpdateAvatar()
+  const updateUserProfile = useUpdateUser()
+  const onSubmit = (data: IUserForm) => {
+    updateUserProfile.mutate(data)
+  }
   const handleEditAvatar = () => {
     ImagePicker.launchImageLibrary(
       { mediaType: 'photo', includeBase64: true },
@@ -74,62 +84,68 @@ const ProfileScreen: React.FC<ISettingsScreenProps> = ({ navigation }) => {
   }
   return (
     <AppLayout backButton={true} navigation={navigation}>
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <View style={styles.container}>
-          <View style={styles.avatarContainer}>
-            <Avatar
-              size={150}
-              rounded
-              title={!data?.avatar ? data?.name.charAt(0) : undefined}
-              containerStyle={
-                !data!.avatar
-                  ? {
-                      backgroundColor: '#3d4db7',
-                      borderRadius: 150,
-                    }
-                  : undefined
-              }
-              source={data!.avatar ? { uri: data!.avatar } : undefined}
-            >
-              <Avatar.Accessory
-                size={40}
-                name="pencil"
-                type="material-community"
-                onPress={handleEditAvatar}
-              ></Avatar.Accessory>
-            </Avatar>
+      <ScrollView>
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <View style={styles.container}>
+            <View style={styles.avatarContainer}>
+              <Avatar
+                size={150}
+                rounded
+                title={!data?.avatar ? data?.name.charAt(0) : undefined}
+                containerStyle={
+                  !data!.avatar
+                    ? {
+                        backgroundColor: '#3d4db7',
+                        borderRadius: 150,
+                      }
+                    : undefined
+                }
+                source={data!.avatar ? { uri: data!.avatar } : undefined}
+              >
+                <Avatar.Accessory
+                  size={40}
+                  name="pencil"
+                  type="material-community"
+                  onPress={handleEditAvatar}
+                />
+              </Avatar>
 
-            <Typography.BodyHeavy>{data?.name}</Typography.BodyHeavy>
-            <Typography.CaptionLight>{`@${data?.name}`}</Typography.CaptionLight>
+              <Typography.BodyHeavy>{data?.name}</Typography.BodyHeavy>
+              <Typography.CaptionLight>{`@${data?.name}`}</Typography.CaptionLight>
+            </View>
+            <Input
+              control={control}
+              name="name"
+              placeholder={t('common:name')}
+              error={!!errors.name}
+              errorMessage={errors.name?.message}
+              returnKeyType="next"
+              autoCapitalize="none"
+              rightIcon={<Icon name="account-outline" />}
+              containerStyle={{ marginBottom: 5 }}
+            />
+            <View style={styles.buttonContainer}>
+              <Button
+                loading={updateUserProfile.isLoading}
+                onPress={handleSubmit(onSubmit)}
+              >
+                {t('common:save')}
+              </Button>
+            </View>
+            <View style={styles.buttonContainer}>
+              <Button
+                onPress={confirmDeleteDialog.open}
+                color={AppTheme.colors.error_default}
+              >
+                {t('common:delete_my_account')}
+              </Button>
+            </View>
+            <ConfirmDeleteDialog {...confirmDeleteDialog} />
           </View>
-
-          <Input
-            control={control}
-            name="name"
-            placeholder={t('common:name')}
-            error={!!errors.name}
-            errorMessage={errors.name?.message}
-            returnKeyType="next"
-            autoCapitalize="none"
-            rightIcon={<Icon name="account-outline" />}
-            containerStyle={{ marginBottom: 5 }}
-          />
-          <View style={styles.buttonContainer}>
-            <Button>{t('common:save')}</Button>
-          </View>
-          <View style={styles.buttonContainer}>
-            <Button
-              onPress={confirmDeleteDialog.open}
-              color={AppTheme.colors.error_default}
-            >
-              {t('common:delete_my_account')}
-            </Button>
-          </View>
-          <ConfirmDeleteDialog {...confirmDeleteDialog} />
-        </View>
-      )}
+        )}
+      </ScrollView>
     </AppLayout>
   )
 }
